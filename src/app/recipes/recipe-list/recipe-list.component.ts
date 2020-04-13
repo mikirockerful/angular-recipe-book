9c37fba58e8193b93f87745b87939faa7fc61492
miguel <gamallomiguel@gmail.com>
2020-04-13 20:40:59 +0200

    Desplay only details for selected recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -7,21 +7,23 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
   recipes: Recipe[] = [
     new Recipe(
       'Lentils',
       'Lentils with puerro',
       'https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Eggplant_Turnovers' +
       '%2C_Tomato_Rice%2C_Lentils_and_Saffron_Rice_at_Moroccan_Soup_Bar.jpg/1280px-Eggplant_Turnovers%2C_Tomato_Rice' +
-      '%2C_Lentils_and_Saffron_Rice_at_Moroccan_Soup_Bar.jpg'
+      '%2C_Lentils_and_Saffron_Rice_at_Moroccan_Soup_Bar.jpg',
+      ['Lentils', 'Puerro']
     ),
      new Recipe(
       'Pizza',
-      'Cheese pizza',
-      'https://i-ticketing.iwos.com/256x256-th/products/221/products_221_17.jpg'
+      'Margherita pizza',
+      'https://i-ticketing.iwos.com/256x256-th/products/221/products_221_17.jpg',
+       ['Cheese', 'Bread', 'Tomato', 'Oregano']
     )
   ];
+  @Output() recipeSelector = new EventEmitter<Recipe>();
 
   constructor() {
   }
@@ -29,4 +31,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  recipeSelected(recipe: Recipe) {
+    this.recipeSelector.emit(recipe);
+  }
 }
