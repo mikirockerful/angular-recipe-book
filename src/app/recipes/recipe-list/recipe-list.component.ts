7e10919982a08f73f8ed34a21578ed05ba602f81
miguel <gamallomiguel@gmail.com>
2020-04-13 20:21:19 +0200

    Extract recipe to its own component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,9 +10,16 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
     new Recipe(
-      'Test title',
-      'Test description',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Eggplant_Turnovers%2C_Tomato_Rice%2C_Lentils_and_Saffron_Rice_at_Moroccan_Soup_Bar.jpg/1280px-Eggplant_Turnovers%2C_Tomato_Rice%2C_Lentils_and_Saffron_Rice_at_Moroccan_Soup_Bar.jpg'
+      'Lentils',
+      'Lentils with puerro',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Eggplant_Turnovers' +
+      '%2C_Tomato_Rice%2C_Lentils_and_Saffron_Rice_at_Moroccan_Soup_Bar.jpg/1280px-Eggplant_Turnovers%2C_Tomato_Rice' +
+      '%2C_Lentils_and_Saffron_Rice_at_Moroccan_Soup_Bar.jpg'
+    ),
+     new Recipe(
+      'Pizza',
+      'Cheese pizza',
+      'https://i-ticketing.iwos.com/256x256-th/products/221/products_221_17.jpg'
     )
   ];
 
