9aeeb93134338ec1d7691b0e99192160ecf6f7e3
miguel <gamallomiguel@gmail.com>
2020-04-19 20:02:44 +0200

    Implement "To shopping list" functionality

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Recipe} from "../recipe.model";
+import {Recipe} from '../recipe.model';
+import {ShoppingListService} from '../../shopping-list/shopping-list.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,10 +10,15 @@ import {Recipe} from "../recipe.model";
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() {
+  constructor(private shoppingListService: ShoppingListService) {
   }
 
   ngOnInit() {
   }
 
+  ingredientsToShoppingList() {
+    for (const ingredient of this.recipe.ingredients) {
+          this.shoppingListService.addIngredient(ingredient);
+    }
+  }
 }
