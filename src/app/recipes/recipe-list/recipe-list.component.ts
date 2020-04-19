f0d2a3a61622b9ccef75b9e65ecbbd4ae216d9c9
miguel <gamallomiguel@gmail.com>
2020-04-19 19:33:39 +0200

    Create services for recipe-item communication

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
+import {RecipeService} from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,31 +8,12 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe(
-      'Lentils',
-      'Lentils with puerro',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Eggplant_Turnovers' +
-      '%2C_Tomato_Rice%2C_Lentils_and_Saffron_Rice_at_Moroccan_Soup_Bar.jpg/1280px-Eggplant_Turnovers%2C_Tomato_Rice' +
-      '%2C_Lentils_and_Saffron_Rice_at_Moroccan_Soup_Bar.jpg',
-      ['Lentils', 'Puerro']
-    ),
-     new Recipe(
-      'Pizza',
-      'Margherita pizza',
-      'https://i-ticketing.iwos.com/256x256-th/products/221/products_221_17.jpg',
-       ['Cheese', 'Bread', 'Tomato', 'Oregano']
-    )
-  ];
-  @Output() recipeSelector = new EventEmitter<Recipe>();
+  recipes: Recipe[];
 
-  constructor() {
+  constructor(private recipeService: RecipeService) {
   }
 
   ngOnInit() {
-  }
-
-  recipeSelected(recipe: Recipe) {
-    this.recipeSelector.emit(recipe);
+    this.recipes = this.recipeService.getRecipes();
   }
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,30 @@
+import {Recipe} from './recipe.model';
+import {EventEmitter} from '@angular/core';
+
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Lentils',
+      'Lentils with puerro',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Eggplant_Turnovers' +
+      '%2C_Tomato_Rice%2C_Lentils_and_Saffron_Rice_at_Moroccan_Soup_Bar.jpg/1280px-Eggplant_Turnovers%2C_Tomato_Rice' +
+      '%2C_Lentils_and_Saffron_Rice_at_Moroccan_Soup_Bar.jpg',
+      ['Lentils', 'Puerro']
+    ),
+    new Recipe(
+      'Pizza',
+      'Margherita pizza',
+      'https://i-ticketing.iwos.com/256x256-th/products/221/products_221_17.jpg',
+      ['Cheese', 'Bread', 'Tomato', 'Oregano']
+    )
+  ];
+
+  constructor() {
+  }
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
