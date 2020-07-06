a099f2321f1ee826c3a9e0893a0efa8b713b6ec0
miguel <gamallomiguel@gmail.com>
2020-07-06 18:21:46 +0200

    Extract the Auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,31 +4,28 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {AppRoutingModule} from './app-routing.module';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import {AuthComponent} from './auth/auth.component';
 import {RecipesModule} from './recipes/recipes.module';
 import {ShoppingListModule} from './shopping-list/shopping-list.module';
 import {SharedModule} from './shared/shared.module';
 import {CoreModule} from './core.module';
+import {AuthModule} from './auth/auth.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent,
     HeaderComponent,
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
+    AuthModule,
+    BrowserModule,
+    CoreModule,
     HttpClientModule,
     RecipesModule,
-    ShoppingListModule,
     SharedModule,
-    CoreModule
+    ShoppingListModule
   ],
   bootstrap: [AppComponent]
 })
--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,16 +1,17 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {AuthComponent} from './auth.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/recipes', pathMatch: 'full'}
+  {path: 'auth', component: AuthComponent}
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forChild(routes)
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
+export class AuthRoutingModule {
 
 }
